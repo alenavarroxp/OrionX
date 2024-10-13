8910066278bc0f5488a13580c2d6ee153852f6ba
alenavarroxp <alejandropvp13@gmail.com>
2024-10-13 23:38:24 +0200

    IA Buttons implemented and functionality to send message to API

--- src/App.tsx
+++ src/App.tsx
@@ -17,27 +17,27 @@ function App() {
   }, []);
 
   return (
-    <div className="bg-gray-800 w-screen h-screen flex flex-col overflow-hidden font-onest">
-
-      <div className="scrollbar relative w-full h-full bg-gray-800 bg-opacity-40 flex">
+    <div className=" w-screen h-screen flex flex-col overflow-hidden font-onest">
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className="scrollbar relative w-full h-full bg-opacity-40 flex">
         <div className="flex flex-col justify-between h-full w-2/3 ">
           <OrionXTitle />
           <OrionXChat />
         </div>
-        <canvas id="canvas" className="w-1/3 h-full bg-gray-800 pointer-events-none">
+        <canvas id="canvas" className="w-1/3 h-full  pointer-events-none">
         </canvas>
         <div className="absolute top-0 right-0 bg-transparent bg-opacity-40 w-1/3 h-full">
           <div className=" flex h-full items-end justify-center">
             <div className="gap-2 flex flex-col mb-8 items-center">
-              <ButtonIA />
-              <ButtonIA />
-              <ButtonIA />
+              <ButtonIA prompt="Suggest a creative question about the future of technology in daily life. In less 15 words" />
+              <ButtonIA prompt="Propose a fun question about the mysteries of the ocean and its creatures. In 15 words" />
+              <ButtonIA prompt="Generate an intriguing question about how art influences culture and society today. In 15 words" />
             </div>
           </div>
         </div>
       </div>
 
-      <div className="z-10 w- p-4 bg-gray-800">
+      <div className="z-10 w- p-4">
         <div className="flex space-x-2 w-2/3">
           <ChatInput />
         </div>
--- src/components/ButtonIA.tsx
+++ src/components/ButtonIA.tsx
@@ -1,26 +1,103 @@
+import { useAtom } from "jotai";
 import { useEffect, useState } from "react";
 import { geminiAPI } from "../api/gemini";
+import { Message, messagesAtom } from "../atoms/messageAtom";
+import { setAnimation } from "../babylon/entity/assistant";
+import { destroyTalkDots, setTalkDots } from "../babylon/interface/elements";
+import { orionXTalk } from "./orionX/utils/OrionX";
 
-export default function ButtonIA() {
-    const [text, setText] = useState<string | undefined>("");
+interface ButtonIAProps {
+    prompt: string;
+}
+
+export default function ButtonIA({ prompt }: ButtonIAProps) {
+    const [messages, setMessages] = useAtom(messagesAtom);
+    const [text, setText] = useState<string>("");
 
     useEffect(() => {
         const fetchResponse = async () => {
-            const prompt = "Generate a unique and intriguing question I could ask AI about techologies, no more than 20 words it is important.";
+            let response;
+            let attempts = 0;
+
+            while (attempts < 5) {
+                try {
+                    response = await geminiAPI(prompt);
+                    if (response && response !== "The AI model is not available to respond to that prompt. Please try again with a different prompt.") {
+                        setText(response);
+                        return;
+                    }
+                } catch (error) {
+                    console.error("Error fetching response:", error);
+                }
+
+                attempts++;
+                await new Promise(resolve => setTimeout(resolve, 2000)); 
+            }
+
+           
+            setText("");
+        };
+
+        fetchResponse();
+    }, [prompt]);
+
+    const sendMessage = async () => {
+        let response;
+        let attempts = 0;
+
+        while (attempts < 5) {
             try {
-                const response = await geminiAPI(prompt);
-                setText(response);
+                response = await geminiAPI(prompt);
+                if (response && response !== "The AI model is not available to respond to that prompt. Please try again with a different prompt.") {
+                    setText(response);
+                    return;
+                }
             } catch (error) {
                 console.error("Error fetching response:", error);
             }
-        };
-        setTimeout(() => {
-            fetchResponse();
-        }, 2000);
-    }, []);
+
+            attempts++;
+            await new Promise(resolve => setTimeout(resolve, 2000));
+        }
+
+        setText("");
+    }
+
+    const handleClick = async () => {
+        try {
+            setTalkDots();
+            sendMessage();
+            const response = await geminiAPI(text);
+            setAnimation("Character|Audio_2");
+            destroyTalkDots();
+
+            if (!response) return;
+
+            const newMessage: Message = {
+                id: messages.length + 1,
+                text: response,
+                sender: "OrionX",
+                replyTo: text,
+                date: new Date(),
+                isTyping: true,
+            };
+
+            setTimeout(() => {
+                setMessages((prevMessages) => [
+                    ...prevMessages,
+                    { ...newMessage, text: "" }
+                ]);
+                orionXTalk(newMessage, setMessages);
+            }, 500);
+
+        } catch (error) {
+            console.error("Error al obtener la respuesta de Gemini:", error);
+            destroyTalkDots();
+        }
+    };
 
     return (<>
-        {text && <div className="bg-gradient-to-r from-sky-600 to-blue-700 w-fit h-fit p-2 rounded-xl text-white">{text}</div>}
+        {text && <div className="bg-gradient-to-r from-sky-600 to-blue-700  w-full h-fit p-2 rounded-xl text-white hover:scale-105 transition cursor-pointer" onClick={handleClick}>{text}</div>}
     </>
 
     )
